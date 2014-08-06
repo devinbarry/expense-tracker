2a4c73d7adf6106c55407268dbb2d6112fa8e7e0
Devin Barry <devinbarry@gmail.com>
2014-08-07 01:06:18 +1200

    Add the primary view for the frontend app.

--- expense_tracker/frontend/views.py
+++ expense_tracker/frontend/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+
+def index(request):
+    """ This application only has one view because it's an AJAX app """
+    return render_to_response('index.html', {}, context_instance=RequestContext(request))
+

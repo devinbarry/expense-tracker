ddef9e463e20e0854afcdb31b82730eedaeed78e
Devin Barry <devinbarry@gmail.com>
2014-08-07 01:06:41 +1200

    Add urls.py for the expense app.

--- expense_tracker/expense_tracker/urls.py
+++ expense_tracker/expense_tracker/urls.py
@@ -1,6 +1,15 @@
+# coding=utf-8
+from __future__ import absolute_import, unicode_literals, print_function, division
+from tastypie.api import Api
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
+from expense.resources import ExpenseResource, UserResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(ExpenseResource())
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +17,16 @@ urlpatterns = patterns('',
     # url(r'^$', 'expense_tracker.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'frontend.views.index'),
+    url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+# Static files should always be served by Apache/Nginx/Other
+# But for development purposes they can just be served by Django
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, }),
+    )
